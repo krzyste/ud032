e3cae2fb35199383febf9834af272118a39a16f3
krzyste <krzyste@gmail.com>
2014-11-30 12:57:16 +0100

    Add solution to Lessson2.08-Handling_Attributes

--- Lesson_2_Data_in_More_Complex_Formats/08-Handling_Attributes/authors.py
+++ Lesson_2_Data_in_More_Complex_Formats/08-Handling_Attributes/authors.py
@@ -3,8 +3,7 @@
 # and add it to a list, one item for an author.
 # See the provided data structure for the expected format.
 # The tags for first name, surname and email should map directly
-# to the dictionary keys, but you have to extract the attributes from the "insr" tag
-# and add them to the list for the dictionary key "insr"
+# to the dictionary keys
 import xml.etree.ElementTree as ET
 
 article_file = "exampleResearchArticle.xml"
@@ -25,7 +24,13 @@ def get_authors(root):
                 "insr": []
         }
 
-        # YOUR CODE HERE
+        for elem in author:
+            for key in data.iterkeys():
+                if elem.tag == key:
+                    if elem.tag == "insr":
+                        data[key].append(elem.attrib["iid"])
+                    else:
+                        data[key] = elem.text
 
         authors.append(data)
 
