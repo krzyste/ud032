a676f096d4a011d1b7dd78765719a2108b91e9fe
krzyste <krzyste@gmail.com>
2014-12-06 19:46:57 +0100

    Update template for process.py

--- Lesson_2_Problem_Set/03-Processing_All/process.py
+++ Lesson_2_Problem_Set/03-Processing_All/process.py
@@ -8,6 +8,19 @@
 # There are couple of helper functions to deal with the data files.
 # Please do not change them for grading purposes.
 # All your changes should be in the 'process_file' function
+# This is example of the datastructure you should return
+# Each item in the list should be a dictionary containing all the relevant data
+# Note - year, month, and the flight data should be integers
+# You should skip the rows that contain the TOTAL data for a year
+# data = [{"courier": "FL",
+#         "airport": "ATL",
+#         "year": 2012,
+#         "month": 12,
+#         "flights": {"domestic": 100,
+#                     "international": 100}
+#         },
+#         {"courier": "..."}
+# ]
 from bs4 import BeautifulSoup
 from zipfile import ZipFile
 import os
@@ -57,12 +70,15 @@ def test():
     data = []
     for f in files:
         data += process_file(f)
-    assert len(data) == 3
+    assert len(data) == 399
     for entry in data[:3]:
         assert type(entry["year"]) == int
         assert type(entry["flights"]["domestic"]) == int
         assert len(entry["airport"]) == 3
         assert len(entry["courier"]) == 2
+    assert data[-1]["airport"] == "ATL"
+    assert data[-1]["flights"] == {'international': 108289, 'domestic': 701425}
+    
     print "... success!"
 
 if __name__ == "__main__":
